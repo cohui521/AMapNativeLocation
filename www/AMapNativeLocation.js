290e657a562c3437c04c806e1d6ec3bf4deebafc
elliven_cch <chunhuicch@126.com>
2018-06-06 16:26:14 +0800

    代码优化

--- www/AMapNativeLocation.js
+++ www/AMapNativeLocation.js
@@ -1,5 +1,9 @@
 var exec = require('cordova/exec');
 
-exports.coolMethod = function (arg0, success, error) {
-    exec(success, error, 'AMapNativeLocation', 'coolMethod', [arg0]);
+var AMapNativeLocation = {};
+
+AMapNativeLocation.getNativeLocation = function (success, error) {
+    exec(success, error, 'AMapNativeLocation', 'getNativeLocation', []);
 };
+
+module.exports = AMapNativeLocation;
